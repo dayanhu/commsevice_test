3b5958a7c3426eb5ff99d9df96164cf2d8c7eac2
zyz <forever_izz@163.com>
2017-03-03 19:59:42 +0800

    config

--- config.py
+++ config.py
@@ -1,16 +1,28 @@
+import configparser
+import os
 
+def get_conf():
+    conf_file = configparser.ConfigParser()
 
-HTTP = 'http://10.201.102.10:8068'
-compare_image = '/compareImg/'
-img = "/img/"
+#os.path.join(path,name)   #连接目录与文件名或目录 结果为path/name
+    conf_file.read(os.path.join(os.getcwd(),'conf.ini'))
 
-def get_http():
-    return HTTP
+    conf = {}
+    conf["lg_url"] = conf_file.get("login","url")
 
+    conf["info_url"] = conf_file.get("userinfo","url")
 
-def get_compare_image():
-    return compare_image
+    conf['sender'] = conf_file.get("email","sender")
 
+    conf['receiver'] = conf_file.get("email","receiver")
 
-def get_img():
-    return img
+    conf['smtpserver'] = conf_file.get("email","smtpserver")
+
+    conf['username'] = conf_file.get("email","username")
+
+    conf['password'] = conf_file.get("email","password") 
+    print(conf)
+    return conf
+
+if __name__ == "__main__":
+    get_conf()
